5687587f7d4fce439e200242bb23eb625df0c3d8
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-10 10:21:35 +0000

    hello saad

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 console.log('hello world');
 
 console.log('what day is it today');
+
+function sayHello(name) {
+    return 'Hello ${name}'
+}
+
+console.log(hello('saad'))
+
+
+console.log('hello saad')
